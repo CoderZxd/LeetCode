6140e43d705cdfefa6453fdea28e8a97ce41f33c
Admin <zxd_0825@163.com>
2018-09-17 01:19:06 +0800

    最长回文子串

---
+++ src/main/java/com/zxd/test/leetcode/Palindrome.java
@@ -0,0 +1,29 @@
+package com.zxd.test.leetcode;
+
+/**
+ * @author CoderZZ
+ * @Title: ${FILE_NAME}
+ * @Project: LeetCode
+ * @Package com.zxd.test.leetcode
+ * @description: TODO:一句话描述信息
+ * @Version 1.0
+ * @create 2018-09-17 1:18
+ **/
+
+//最长回文子串
+//给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为1000。
+//
+//        示例 1：
+//
+//        输入: "babad"
+//        输出: "bab"
+//        注意: "aba"也是一个有效答案。
+//        示例 2：
+//
+//        输入: "cbbd"
+//        输出: "bb"
+public class Palindrome {
+    public String longestPalindrome(String s) {
+        return "";
+    }
+}
