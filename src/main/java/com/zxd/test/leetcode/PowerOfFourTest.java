356f139a65273e25e51d8422cdd26470d0b64919
xiaodong.zou <zxd_0825@163.com>
2019-09-18 23:02:47 +0800

    "

---
+++ src/main/java/com/zxd/test/leetcode/PowerOfFourTest.java
@@ -0,0 +1,38 @@
+package com.zxd.test.leetcode;
+
+//给定一个整数 (32 位有符号整数)，请编写一个函数来判断它是否是 4 的幂次方。
+//
+//        示例 1:
+//
+//        输入: 16
+//        输出: true
+//        示例 2:
+//
+//        输入: 5
+//        输出: false
+//        进阶：
+//        你能不使用循环或者递归来完成本题吗？
+public class PowerOfFourTest {
+    public static boolean isPowerOfFour(int num) {
+        if((num != 1 && num % 4 != 0) || num == 0){
+            return false;
+        }
+        if(num == 1){
+            return true;
+        }
+        int value = num >>> 2;
+        while(true){
+            if(value != 1 && value % 4 != 0){
+                return false;
+            }else if(value == 1){
+                return true;
+            }else{
+                value = value >>> 2;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PowerOfFourTest.isPowerOfFour(16));
+    }
+}
