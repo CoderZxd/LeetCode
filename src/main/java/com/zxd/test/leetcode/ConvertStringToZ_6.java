8f5c9d212d4f0c11920b9627a44353e362566f59
xiaodong.zou <zxd_0825@163.com>
2019-11-10 20:55:13 +0800

    6

--- src/main/java/com/zxd/test/leetcode/ConvertStringToZ_6.java
+++ src/main/java/com/zxd/test/leetcode/ConvertStringToZ_6.java
@@ -8,36 +8,41 @@ import java.util.List;
  * @Title: ${FILE_NAME}
  * @Project: LeetCode
  * @Package com.zxd.test.leetcode
- * @description: Z字形变换
+ * @description: 6.Z字形变换
  * @Version 1.0
  * @create 2018-09-18 0:32
  **/
-//Z字形变换
-//        将字符串 "PAYPALISHIRING" 以Z字形排列成给定的行数：
+//将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
 //
-//        P   A   H   N
-//        A P L S I I G
-//        Y   I   R
-//        之后从左往右，逐行读取字符："PAHNAPLSIIGYIR"
+//        比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
 //
-//        实现一个将字符串进行指定行数变换的函数:
+//        L   C   I   R
+//        E T O E S I I G
+//        E   D   H   N
+//        之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+//
+//        请你实现这个将字符串进行指定行数变换的函数：
 //
 //        string convert(string s, int numRows);
-//        示例 1:
+//        示例 1:
 //
-//        输入: s = "PAYPALISHIRING", numRows = 3
-//        输出: "PAHNAPLSIIGYIR"
-//        示例 2:
+//        输入: s = "LEETCODEISHIRING", numRows = 3
+//        输出: "LCIRETOESIIGEDHN"
+//        示例 2:
 //
-//        输入: s = "PAYPALISHIRING", numRows = 4
-//        输出: "PINALSIGYAHRPI"
+//        输入: s = "LEETCODEISHIRING", numRows = 4
+//        输出: "LDREOEIIECIHNTSG"
 //        解释:
 //
-//        P     I    N
-//        A   L S  I G
-//        Y A   H R
-//        P     I
-public class ConvertStringToZ {
+//        L     D     R
+//        E   O E   I I
+//        E C   I H   N
+//        T     S     G
+//
+//        来源：力扣（LeetCode）
+//        链接：https://leetcode-cn.com/problems/zigzag-conversion
+//        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+public class ConvertStringToZ_6 {
 
     public static String convert(String s, int numRows) {
         List<StringBuffer> rows = new ArrayList<StringBuffer>();
