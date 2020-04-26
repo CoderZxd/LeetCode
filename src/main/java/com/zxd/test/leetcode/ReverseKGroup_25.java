35348fbe2f553376d9c7dac7ec03080664bcf628
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-04-26 23:04:21 +0800

    25递归方法

--- src/main/java/com/zxd/test/leetcode/ReverseKGroup_25.java
+++ src/main/java/com/zxd/test/leetcode/ReverseKGroup_25.java
@@ -58,10 +58,13 @@ public class ReverseKGroup_25 {
 		l1_3.next = l1_4;
 		l1_2.next = l1_3;
 		l1.next = l1_2;
-		reverseKGroup(l1,3);
+		reverseKGroup(l1,6);
+		ListNode re = reverseKGroup_2(l1,6);
+		System.out.println(re);
 	}
 
 	/**
+	 * 非递归
 	 * 将head根据k进行切割，然后将切割后的节点全部进行反转后再拼接一起
 	 * @param head
 	 * @param k
@@ -165,5 +168,44 @@ public class ReverseKGroup_25 {
 		return resultNodes.get(0);
 	}
 
+	/**
+	 * 递归方式
+	 * @param head
+	 * @param k
+	 * @return
+	 */
+	public static ListNode reverseKGroup_2(ListNode head, int k) {
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode pre = dummy;
+		ListNode end = dummy;
+		while (end.next != null) {
+			for (int i = 0; i < k && end != null; i++) {
+				end = end.next;
+			}
+			if(end == null){
+				break;
+			}
+			ListNode start = pre.next;
+			ListNode next = end.next;
+			end.next = null;
+			pre.next = reverse(start);
+			start.next = next;
+			pre = start;
+			end = pre;
+		}
+		return dummy.next;
+	}
 
+	private static ListNode reverse(ListNode head) {
+		ListNode pre = null;
+		ListNode curr = head;
+		while (curr != null) {
+			ListNode next = curr.next;
+			curr.next = pre;
+			pre = curr;
+			curr = next;
+		}
+		return pre;
+	}
 }
