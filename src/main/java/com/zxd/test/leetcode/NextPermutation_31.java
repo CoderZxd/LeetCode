227bb8eb3752210d87816bbf34128c954f2666c9
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-04-29 19:26:38 +0800

    31 commit

--- src/main/java/com/zxd/test/leetcode/NextPermutation_31.java
+++ src/main/java/com/zxd/test/leetcode/NextPermutation_31.java
@@ -2,10 +2,55 @@ package com.zxd.test.leetcode;
 
 /**
  * @Title: NextPermutation_31
- * @Description: TODO
+ * @Description: https://leetcode-cn.com/problems/next-permutation/
  * @Author xiaodong.zou
  * @Date 2020/4/29 9:11
  */
+//31. 下一个排列
+//		实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+//
+//		如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+//
+//		必须原地修改，只允许使用额外常数空间。
+//
+//		以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+//		1,2,3 → 1,3,2
+//		3,2,1 → 1,2,3
+//		1,1,5 → 1,5,1
+//		1,3,2 → 2,1,3
 public class NextPermutation_31 {
 
+	public static void main(String[] args) {
+		nextPermutation(new int[]{1,3,2});
+	}
+
+	public static void nextPermutation(int[] nums) {
+		if(nums != null && nums.length > 1){
+			int len = nums.length;
+			boolean swap = false;
+			label:
+			for(int end=len-1;end >=0;end--){
+				for(int pre=end-1;pre>=0;pre--){
+					if(nums[end]>nums[pre]){
+						swap = true;
+						while (end > pre){
+							int temp = nums[end-1];
+							nums[end-1] = nums[end];
+							nums[end] = temp;
+							end--;
+						}
+						break label;
+					}
+				}
+			}
+			//如果没有交换过，说明没有更大的排列,排列成最小排列(头尾两两交换)
+			if(!swap){
+				for(int end = len-1,start=0;start<end;start++,end--){
+					int temp = nums[start];
+					nums[start] = nums[end];
+					nums[end] = temp;
+				}
+			}
+		}
+	}
 }
