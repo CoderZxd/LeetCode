135b687d123481bede9d96fb86be3baaf22daa87
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-05-07 19:59:41 +0800

    39 first commit

--- src/main/java/com/zxd/test/leetcode/CombinationSum_39.java
+++ src/main/java/com/zxd/test/leetcode/CombinationSum_39.java
@@ -1,5 +1,7 @@
 package com.zxd.test.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -38,11 +40,37 @@ import java.util.List;
 public class CombinationSum_39 {
 
     public static void main(String[] args) {
-
-
+        combinationSum(new int[]{2,3,5},8);
     }
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return null;
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> resultList = new ArrayList<>(10);
+        int len = candidates.length;
+        for(int i=0;i<len;i++){
+            List<Integer> tempList = new ArrayList<>(10);
+            deal(tempList,target,candidates,i);
+            if(tempList.isEmpty()){
+               continue;
+            }
+            resultList.add(tempList);
+        }
+        return resultList;
+    }
+
+    private static void deal(List<Integer> temp,int target,int[] candidates,int index){
+        int len = candidates.length;
+        for(int i=0;i<len;i++){
+            if(candidates[i] == target){
+                temp.add(target);
+                return;
+            }
+        }
+        int tempTarget = target - candidates[index];
+        if(tempTarget < 0){
+            temp.clear();
+            return;
+        }
+        temp.add(candidates[index]);
+        deal(temp,tempTarget,candidates,index);
     }
 }
