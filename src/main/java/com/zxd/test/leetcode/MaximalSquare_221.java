dfb6a40ef7644da8669d8c718f981f5fe057dc25
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-05-08 20:28:03 +0800

    221 dp提交

--- src/main/java/com/zxd/test/leetcode/MaximalSquare_221.java
+++ src/main/java/com/zxd/test/leetcode/MaximalSquare_221.java
@@ -146,7 +146,7 @@ public class MaximalSquare_221 {
 		int[][] dp = new int[rows][columns];
 		for(int i=0;i<rows;i++){
 			for(int j=0;j<columns;j++){
-				if(matrix[i][j] == 1){
+				if(matrix[i][j] == '1'){
 					if(i==0 || j==0){
 						dp[i][j] = 1;
 					}else{
