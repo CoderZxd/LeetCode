40c880779ebc808d2214b1ab23b3d1b5430449cd
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-05-11 15:45:18 +0800

    236 commit

--- src/main/java/com/zxd/test/leetcode/LowestCommonAncestor_236.java
+++ src/main/java/com/zxd/test/leetcode/LowestCommonAncestor_236.java
@@ -1,8 +1,7 @@
 package com.zxd.test.leetcode;
 
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * @ClassName LowestCommonAncestor_236
@@ -65,6 +64,8 @@ public class LowestCommonAncestor_236 {
         afterfs(treeNode_3);
         System.out.println("-----------------------------");
         dfs(treeNode_3);
+        System.out.println("=====================================");
+        lowestCommonAncestor(treeNode_3,new TreeNode(5),new TreeNode(4));
     }
 
     /**
@@ -76,10 +77,97 @@ public class LowestCommonAncestor_236 {
      *     TreeNode(int x) { val = x; }
      * }
      */
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        return null;
+    /**
+     * 通过广度遍历，找出节点与节点之前的父子关系(一个父节点的两个子节点位置为2n+1,2n+2)，2n+1即奇数一定是左节点，2n+2即偶数一定是右节点
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Map<Integer,Integer> valToPosMap = new HashMap<>(16);
+        Map<Integer,TreeNode> posToTreeNodeMap = new HashMap<>(16);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        valToPosMap.put(root.val,0);
+        posToTreeNodeMap.put(0,root);
+        // 先进先出
+        while (!queue.isEmpty()) {
+            TreeNode tempTreeNode = queue.remove();
+            if (tempTreeNode.left != null){
+                queue.add(tempTreeNode.left);
+                valToPosMap.put(tempTreeNode.left.val,valToPosMap.get(tempTreeNode.val)*2+1);
+                posToTreeNodeMap.put(valToPosMap.get(tempTreeNode.val)*2+1,tempTreeNode.left);
+            }
+            if (tempTreeNode.right != null){
+                queue.add(tempTreeNode.right);
+                valToPosMap.put(tempTreeNode.right.val,valToPosMap.get(tempTreeNode.val)*2+2);
+                posToTreeNodeMap.put(valToPosMap.get(tempTreeNode.val)*2+2,tempTreeNode.right);
+            }
+        }
+        System.out.println("valToPosMap---->"+valToPosMap);
+        //找出节点所有的父节点位置，找到离该节点最近的相同的节点即为最近父节点
+        //找节点p所有的父节点位置
+        int posP = valToPosMap.get(p.val);
+        System.out.println("posP----->"+posP);
+        List<Integer> parentsP = new ArrayList<>(10);
+        //将P的位置加入
+        parentsP.add(posP);
+        getParentsPos(posP,parentsP);
+        System.out.println("parentsP---->"+parentsP);
+        int posQ = valToPosMap.get(q.val);
+        System.out.println("posQ----->"+posQ);
+        List<Integer> parentsQ = new ArrayList<>(10);
+        //将Q的位置加入
+        parentsQ.add(posQ);
+        getParentsPos(posQ,parentsQ);
+        System.out.println("parentsQ---->"+parentsQ);
+        //找到parentsP和parentsQ最大的相同数即为最近的父节点所在的位置
+        int commonParentPos = -1;
+        int parentPSize = parentsP.size();
+        int parentQSize = parentsQ.size();
+        int i = 0,j = 0;
+        while (i<parentPSize || j<parentQSize){
+            if(parentsP.get(i).equals(parentsQ.get(j))){
+                commonParentPos = parentsP.get(i);
+                break;
+            }
+            if(parentsP.get(i) > parentsQ.get(j)){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        System.out.println("commonParentPos===="+commonParentPos);
+        //将该位置的节点进行返回
+        TreeNode treeNode = posToTreeNodeMap.get(commonParentPos);
+        return treeNode;
     }
+
+    /**
+     * 获取父节点位置列表
+     * @param pos
+     * @param parents
+     */
+    private static void getParentsPos(int pos, List<Integer> parents){
+        if(pos%2==0){
+            int parent =  (pos-2)/2;
+            if(parent < 0){
+                return;
+            }
+            parents.add(parent);
+            getParentsPos(parent,parents);
+        }
+        if(pos%2==1){
+            int parent =  (pos-1)/2;
+            if(parent < 0){
+                return;
+            }
+            parents.add(parent);
+            getParentsPos(parent,parents);
+        }
+    }
+
 
     /**
      * 前序遍历
@@ -139,9 +227,6 @@ public class LowestCommonAncestor_236 {
             }
         }
     }
-
-
-
 }
 
 class TreeNode {
