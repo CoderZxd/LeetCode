81db6e6f3fba27dde239b5a9f59462bdc855072d
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-05-12 15:01:58 +0800

    50 commit

--- src/main/java/com/zxd/test/leetcode/MyPow_50.java
+++ src/main/java/com/zxd/test/leetcode/MyPow_50.java
@@ -30,11 +30,11 @@ public class MyPow_50 {
 
 	public static void main(String[] args) {
 		System.out.println(myPow(2.0,10));
+		System.out.println(myPow_2(2.0,10));
 	}
 
 	//方法一：快速幂 + 递归
 	public static double myPow(double x, int n) {
-		long result = n;
 		return n>=0?quickMul(x,n):1.0/quickMul(x,-n);
 	}
 
@@ -44,5 +44,28 @@ public class MyPow_50 {
 		}
 		double y = quickMul(x,n/2);
 		return n%2 == 0?y*y:y*y*x;
+	}
+
+//	方法二：快速幂 + 迭代
+	public static double myPow_2(double x, int n) {
+		return n>=0?quickMul_2(x,n):1.0/quickMul_2(x,-n);
+	}
+
+	private static double quickMul_2(double x,int n){
+		double ans = 1.0;
+		// 贡献的初始值为 x
+		double x_contribute = x;
+		// 在对 N 进行二进制拆分的同时计算答案
+		while (n > 0) {
+			if (n % 2 == 1) {
+				// 如果 N 二进制表示的最低位为 1，那么需要计入贡献
+				ans *= x_contribute;
+			}
+			// 将贡献不断地平方
+			x_contribute *= x_contribute;
+			// 舍弃 N 二进制表示的最低位，这样我们每次只要判断最低位即可
+			n /= 2;
+		}
+		return ans;
 	}
 }
