46f80ad986dba64a61d68fb7eed8814cee9e79c5
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-05-14 15:59:32 +0800

    136 commit

--- src/main/java/com/zxd/test/leetcode/SingleNumber_136.java
+++ src/main/java/com/zxd/test/leetcode/SingleNumber_136.java
@@ -41,4 +41,17 @@ public class SingleNumber_136 {
 		}
 		return tempMap.keySet().iterator().next();
 	}
+
+//	可使用异或运算⊕。异或运算有以下三个性质。
+//
+//	任何数和 0 做异或运算，结果仍然是原来的数，即a⊕0=a。
+//	任何数和其自身做异或运算，结果是 0，即a⊕a=0。
+//	异或运算满足交换律和结合律，即a⊕b⊕a=b⊕a⊕a=b⊕(a⊕a)=b⊕0=b。
+	public static int singleNumber_2(int[] nums) {
+		int result = 0;
+		for(int ele:nums){
+			result ^= ele;
+		}
+		return result;
+	}
 }
