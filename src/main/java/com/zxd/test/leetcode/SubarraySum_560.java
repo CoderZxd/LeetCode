34c2ce94d8e0a8ceecb24af15978d7e922b81331
xiaodong.zou <zxd_0825@163.com>
2020-05-15 22:59:54 +0800

    560 method 2 commit

--- src/main/java/com/zxd/test/leetcode/SubarraySum_560.java
+++ src/main/java/com/zxd/test/leetcode/SubarraySum_560.java
@@ -1,5 +1,7 @@
 package com.zxd.test.leetcode;
 
+import java.util.HashMap;
+
 /**
  * @Title: SubarraySum_560
  * @Description: https://leetcode-cn.com/problems/subarray-sum-equals-k/
@@ -21,14 +23,14 @@ public class SubarraySum_560 {
 
 	public static void main(String[] args) {
 		System.out.println(subarraySum(new int[]{28,54,7,-70,22,65,-6},100));
+		System.out.println(subarraySum_2(new int[]{3,4,7,2,-3,1,4,2},7));
 	}
 
 	public static int subarraySum(int[] nums, int k) {
 		int result = 0;
-		int temp = k;
 		int length = nums.length;
 		for(int i=0;i<length;i++){
-			temp = k;
+			int temp = k;
 			for (int j=i;j<length;j++){
 				temp -= nums[j];
 				if(temp == 0){
@@ -38,5 +40,19 @@ public class SubarraySum_560 {
 			}
 		}
 		return result;
+	}
+
+	//方法二：前缀和 + 哈希表优化
+	public static int subarraySum_2(int[] nums, int k) {
+		int count = 0, pre = 0;
+		HashMap <Integer,Integer> mp = new HashMap<>(16);
+		mp.put(0, 1);
+		for (int i = 0; i < nums.length; i++) {
+			pre += nums[i];
+			if (mp.containsKey(pre - k))
+				count += mp.get(pre - k);
+			mp.put(pre, mp.getOrDefault(pre,0) + 1);
+		}
+		return count;
 	}
 }
