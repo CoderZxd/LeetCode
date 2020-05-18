c13400619a2ccb11ad2a72c357b987831191e960
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-05-18 17:27:26 +0800

    152 dp commit

--- src/main/java/com/zxd/test/leetcode/MaxProduct_152.java
+++ src/main/java/com/zxd/test/leetcode/MaxProduct_152.java
@@ -25,6 +25,7 @@ public class MaxProduct_152 {
 
 	public static void main(String[] args) {
 		System.out.println(maxProduct(new int[]{-2,0,-1}));
+		System.out.println(maxProduct_dp(new int[]{-2,0,-1}));
 	}
 
 	public static int maxProduct(int[] nums) {
@@ -43,5 +44,20 @@ public class MaxProduct_152 {
 			}
 		}
 		return result;
+	}
+
+//方法一：动态规划
+	public static int maxProduct_dp(int[] nums) {
+		int[] maxF = new int[nums.length];
+		int[] minF = new int[nums.length];
+		maxF[0] = nums[0];
+		minF[0] = nums[0];
+		int ans = nums[0];
+		for(int i=1;i<nums.length;i++){
+			maxF[i] = Math.max(maxF[i-1]*nums[i],Math.max(nums[i],minF[i-1]*nums[i]));
+			minF[i] = Math.min(minF[i-1]*nums[i],Math.min(nums[i],maxF[i-1]*nums[i]));
+			ans = Math.max(ans,maxF[i]);
+		}
+		return ans;
 	}
 }
