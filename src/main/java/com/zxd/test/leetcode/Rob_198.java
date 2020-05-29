bf6e700eb7ebbd40d5dc351edb3b438e1210437f
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-05-29 14:01:38 +0800

    198 dp commit

--- src/main/java/com/zxd/test/leetcode/Rob_198.java
+++ src/main/java/com/zxd/test/leetcode/Rob_198.java
@@ -42,25 +42,21 @@ public class Rob_198 {
 	 * @return
 	 */
 	public static int rob_1(int[] nums) {
-		int result = 0;
+		if(nums == null || nums.length == 0){
+			return 0;
+		}
 		int len = nums.length;
-		if(len == 0){
-			return result;
+		if(len == 1){
+			return nums[0];
 		}
 		int[] dp = new int[len];
 		dp[0] = nums[0];
-		result = nums[0];
-		if(len>=2){
-			dp[1] = Math.max(nums[0],nums[1]);
-			result = dp[1];
-		}
+		dp[1] = Math.max(nums[0],nums[1]);
 		for(int i=2;i<len;i++){
 			dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
-			if(dp[i]>result){
-				result = dp[i];
-			}
+
 		}
-		return result;
+		return dp[len-1];
 	}
 
 	public static int rob(int[] nums) {
