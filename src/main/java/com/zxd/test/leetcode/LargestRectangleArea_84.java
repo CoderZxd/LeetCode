c71b7542e1d51693df6c99c383d709e138083de2
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-05-30 15:56:18 +0800

    84 单调栈解法 commit

--- src/main/java/com/zxd/test/leetcode/LargestRectangleArea_84.java
+++ src/main/java/com/zxd/test/leetcode/LargestRectangleArea_84.java
@@ -1,5 +1,8 @@
 package com.zxd.test.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @Title: LargestRectangleArea_84
  * @Description: https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
@@ -33,6 +36,7 @@ public class LargestRectangleArea_84 {
 
 	public static void main(String[] args) {
 		System.out.println(largestRectangleArea(new int[]{2,1,5,6,2,3}));
+		System.out.println(largestRectangleArea_stack(new int[]{2,1,5,6,2,3}));
 	}
 
 	/**
@@ -55,5 +59,52 @@ public class LargestRectangleArea_84 {
 			maxArea = Math.max(maxArea,h*w);
 		}
 		return maxArea;
+	}
+	/**
+	 * 方法二:单调栈
+	 * @param heights
+	 * @return
+	 */
+	public static int largestRectangleArea_stack(int[] heights) {
+		int len = heights.length;
+		if(len == 0){
+			return 0;
+		}
+		if(len == 1){
+			return heights[0];
+		}
+		int area = 0;
+		Deque<Integer> stack = new ArrayDeque<>();
+		for(int i=0;i<len;i++){
+			while (!stack.isEmpty() && heights[stack.peekLast()] > heights[i]){
+				int height = heights[stack.removeLast()];
+				while (!stack.isEmpty() && heights[stack.peekLast()] == height){
+					stack.removeLast();
+				}
+				int width;
+				if(stack.isEmpty()){
+					width = i;
+				}else{
+					width = i - stack.peekLast() - 1;
+				}
+				area = Math.max(area,width*height);
+			}
+			stack.add(i);
+		}
+		while (!stack.isEmpty()){
+			int height = heights[stack.removeLast()];
+			while (!stack.isEmpty() && heights[stack.peekLast()] == height){
+				stack.removeLast();
+			}
+			int width;
+			if(stack.isEmpty()){
+				width = len;
+			}else{
+				width = len - stack.peekLast() - 1;
+			}
+			area = Math.max(area,width*height);
+
+		}
+		return area;
 	}
 }
