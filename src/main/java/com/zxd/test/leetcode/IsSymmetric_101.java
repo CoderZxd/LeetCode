ca0537957bf0a4e0a677e35b1cc37bef08dccc73
xiaodong.zou <zxd_0825@163.com>
2020-05-31 13:23:46 +0800

    101 last commit

--- src/main/java/com/zxd/test/leetcode/IsSymmetric_101.java
+++ src/main/java/com/zxd/test/leetcode/IsSymmetric_101.java
@@ -1,5 +1,9 @@
 package com.zxd.test.leetcode;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @ClassName IsSymmetric_101
  * @Description https://leetcode-cn.com/problems/symmetric-tree/
@@ -36,11 +40,140 @@ package com.zxd.test.leetcode;
 public class IsSymmetric_101 {
 
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2_left = new TreeNode(2);
+        TreeNode node2_right = new TreeNode(2);
+        TreeNode node3_left = new TreeNode(3);
+        TreeNode node4_right = new TreeNode(4);
+        TreeNode node4_left = new TreeNode(4);
+        TreeNode node3_right = new TreeNode(3);
+        node2_left.left = node3_left;
+        node2_left.right = node4_right;
+        root.left = node2_left;
+        node2_right.left = node4_left;
+        node2_right.right = node3_right;
+        root.right = node2_right;
+        System.out.println(isSymmetric(root));
+        TreeNode root2 = new TreeNode(1);
+        TreeNode node2_left_1 = new TreeNode(2);
+        TreeNode node2_left_2 = new TreeNode(2);
+        TreeNode node2_right_1 = new TreeNode(2);
+        TreeNode node2_right_2 = new TreeNode(2);
+        node2_right_1.left = node2_right_2;
+        node2_left_1.left = node2_left_2;
+        root2.left = node2_left_1;
+        root2.right = node2_right_1;
+        System.out.println(isSymmetric(root2));
+//        bfs(root);
+//        System.out.println("===========================");
+//        dfs(root);
+//        System.out.println("===========================");
+//        afs(root);
+//        System.out.println("===========================");
+//        bbfs(root);
 
     }
 
-    public boolean isSymmetric(TreeNode root) {
+
+    public static boolean isSymmetric(TreeNode root) {
+        if(root == null || root.left == root.right){
+            return true;
+        }
+        if(root.left != null && root.right != null && root.left.val != root.right.val ){
+            return false;
+        }
+        return ifSymmetric(root.left,root.right);
+    }
+
+    private static boolean ifSymmetric(TreeNode leftNode, TreeNode rightNode){
+        if(leftNode == rightNode){
+            return true;
+        }
+        if((leftNode == null && rightNode != null) || (leftNode != null && rightNode == null)){
+            return false;
+        }
+        if((leftNode.left == null && rightNode.right != null) || (leftNode.left != null && rightNode.right ==null) ||(leftNode.right == null && rightNode.left != null) ||(leftNode.right != null && rightNode.left == null)){
+            return false;
+        }
+        if((leftNode.left == rightNode.right || leftNode.left.val == rightNode.right.val) && (leftNode.right == rightNode.left || leftNode.right.val == rightNode.left.val)){
+            return ifSymmetric(leftNode.left,rightNode.right) && ifSymmetric(leftNode.right,rightNode.left);
+        }
         return false;
+    }
+
+
+
+
+
+    private static void dfs(TreeNode node,List<Integer> elements){
+        if(node == null){
+            elements.add(null);
+            return;
+        }
+        dfs(node.left,elements);
+        elements.add(node.val);
+        dfs(node.right,elements);
+    }
+
+    private static void bfs(TreeNode node){
+        if(node == null){
+            return;
+        }
+        System.out.println(node.val);
+        bfs(node.left);
+        bfs(node.right);
+    }
+
+    private static void dfs(TreeNode node){
+        if(node == null){
+            return;
+        }
+        dfs(node.left);
+        System.out.println(node.val);
+        dfs(node.right);
+    }
+
+    private static void afs(TreeNode node){
+        if(node == null){
+            return;
+        }
+        afs(node.left);
+        afs(node.right);
+        System.out.println(node.val);
+    }
+
+    private static void bbfs(TreeNode node){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()){
+            TreeNode treeNode = queue.poll();
+            System.out.println(treeNode.val);
+            if(treeNode.left != null){
+                queue.add(treeNode.left);
+            }
+            if(treeNode.right != null){
+                queue.add(treeNode.right);
+            }
+        }
+    }
+
+    private static void bfs(TreeNode node,List<Integer> elements){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()){
+            TreeNode treeNode = queue.poll();
+            if(treeNode != null){
+                elements.add(treeNode.val);
+            }else{
+                elements.add(null);
+            }
+            if(treeNode!= null){
+                queue.add(treeNode.left);
+            }
+            if(treeNode != null){
+                queue.add(treeNode.right);
+            }
+        }
     }
 
     /**
