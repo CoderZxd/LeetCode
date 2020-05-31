e8295e1be2b7f035b71657047e19546021ea1fd6
xiaodong.zou <zxd_0825@163.com>
2020-05-31 10:51:15 +0800

    101 first commit

---
+++ src/main/java/com/zxd/test/leetcode/IsSymmetric_101.java
@@ -0,0 +1,55 @@
+package com.zxd.test.leetcode;
+
+/**
+ * @ClassName IsSymmetric_101
+ * @Description https://leetcode-cn.com/problems/symmetric-tree/
+ * @Author xiaodong.zou
+ * @Date 2020/5/31 10:48
+ * @Version 1.0
+ */
+//101. 对称二叉树
+//        给定一个二叉树，检查它是否是镜像对称的。
+//
+//
+//
+//        例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+//
+//         1
+//        / \
+//       2   2
+//      / \ / \
+//     3  4 4  3
+//
+//
+//        但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+//
+//        1
+//       / \
+//      2   2
+//      \   \
+//      3    3
+//
+//
+//        进阶：
+//
+//        你可以运用递归和迭代两种方法解决这个问题吗？
+public class IsSymmetric_101 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+}
