f6776987f157ff5b84c5f731860d5398ad5b35d7
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-06-03 18:02:55 +0800

    test

--- src/main/java/com/zxd/test/leetcode/Test.java
+++ src/main/java/com/zxd/test/leetcode/Test.java
@@ -1,6 +1,9 @@
 package com.zxd.test.leetcode;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,7 +15,7 @@ import java.util.TimerTask;
  */
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Calendar calendar = Calendar.getInstance();
 //		calendar.add(Calendar.DAY_OF_MONTH,1);
 //		calendar.set(Calendar.HOUR_OF_DAY,7);
@@ -24,6 +27,13 @@ public class Test {
 				System.out.println("==========================>>>>>>>>>>>>>>");
 			}
 		},calendar.getTime(),30*1000L);
+
+		String time = "2019-08-06T14:40:00Z";
+		time = time.replaceAll("T"," ").replaceAll("Z","");
+		SimpleDateFormat dateFm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //格式化当前系统日期
+		String dateTime = dateFm.format(new java.util.Date());
+		Date parse = dateFm.parse(time);
+		System.out.println(dateTime);
 	}
 
 }
