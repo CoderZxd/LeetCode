8a7831979d9e5708bc0b56af546246b3ccc943c2
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-06-05 13:57:29 +0800

    29 for offer add offical answer

--- src/main/java/com/zxd/test/leetcode/SpiralOrder_29_For_Offer.java
+++ src/main/java/com/zxd/test/leetcode/SpiralOrder_29_For_Offer.java
@@ -32,6 +32,8 @@ public class SpiralOrder_29_For_Offer {
 
 	public static void main(String[] args) {
 		spiralOrder(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}});
+		spiralOrder_1(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}});
+		spiralOrder_2(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}});
 		spiralOrder(new int[][]{});
 		spiralOrder(new int[][]{{1},{2}});
 	}
@@ -104,4 +106,92 @@ public class SpiralOrder_29_For_Offer {
 		return result;
 	}
 
+	/**
+	 * 官方题解一：模拟
+	 * @param matrix
+	 * @return
+	 */
+//	可以模拟打印矩阵的路径。初始位置是矩阵的左上角，初始方向是向右，当路径超出界限或者进入之前访问过的位置时，则顺时针旋转，进入下一个方向。
+//
+//	判断路径是否进入之前访问过的位置需要使用一个与输入矩阵大小相同的辅助矩阵 visited，其中的每个元素表示该位置是否被访问过。当一个元素被访问时，将 visited 中的对应位置的元素设为已访问。
+//
+//	如何判断路径是否结束？由于矩阵中的每个元素都被访问一次，因此路径的长度即为矩阵中的元素数量，当路径的长度达到矩阵中的元素数量时即为完整路径，将该路径返回。
+	public static int[] spiralOrder_1(int[][] matrix) {
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+			return new int[0];
+		}
+		int rows = matrix.length, columns = matrix[0].length;
+		boolean[][] visited = new boolean[rows][columns];
+		int total = rows * columns;
+		int[] order = new int[total];
+		int row = 0, column = 0;
+		int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+		int directionIndex = 0;
+		for (int i = 0; i < total; i++) {
+			order[i] = matrix[row][column];
+			visited[row][column] = true;
+			int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+			if (nextRow < 0 || nextRow >= rows || nextColumn < 0 || nextColumn >= columns || visited[nextRow][nextColumn]) {
+				directionIndex = (directionIndex + 1) % 4;
+			}
+			row += directions[directionIndex][0];
+			column += directions[directionIndex][1];
+		}
+		return order;
+	}
+
+	/**
+	 * 官方题解二：按层模拟
+	 * @param matrix
+	 * @return
+	 */
+//	可以将矩阵看成若干层，首先打印最外层的元素，其次打印次外层的元素，直到打印最内层的元素。
+//
+//	定义矩阵的第 kk 层是到最近边界距离为 kk 的所有顶点。例如，下图矩阵最外层元素都是第 11 层，次外层元素都是第 22 层，剩下的元素都是第 33 层。
+//
+//
+//			[[1, 1, 1, 1, 1, 1, 1],
+//			[1, 2, 2, 2, 2, 2, 1],
+//			[1, 2, 3, 3, 3, 2, 1],
+//			[1, 2, 2, 2, 2, 2, 1],
+//			[1, 1, 1, 1, 1, 1, 1]]
+//	对于每层，从左上方开始以顺时针的顺序遍历所有元素。假设当前层的左上角位于(top,left)，右下角位于(bottom,right)，按照如下顺序遍历当前层的元素。
+//
+//	1.从左到右遍历上侧元素，依次为 (top,left) 到 (top,right)。
+//
+//	2.从上到下遍历右侧元素，依次为 (top+1,right) 到(bottom,right)。
+//
+//	3.如果left<right 且 top<bottom，则从右到左遍历下侧元素，依次为(bottom,right−1)到(bottom,left+1)，以及从下到上遍历左侧元素，依次为(bottom,left)到(top+1,left)。
+//
+//	遍历完当前层的元素之后，将 left 和top 分别增加 1，将right 和 bottom 分别减少 1，进入下一层继续遍历，直到遍历完所有元素为止。
+	public static int[] spiralOrder_2(int[][] matrix) {
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+			return new int[0];
+		}
+		int rows = matrix.length, columns = matrix[0].length;
+		int[] order = new int[rows * columns];
+		int index = 0;
+		int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+		while (left <= right && top <= bottom) {
+			for (int column = left; column <= right; column++) {
+				order[index++] = matrix[top][column];
+			}
+			for (int row = top + 1; row <= bottom; row++) {
+				order[index++] = matrix[row][right];
+			}
+			if (left < right && top < bottom) {
+				for (int column = right - 1; column > left; column--) {
+					order[index++] = matrix[bottom][column];
+				}
+				for (int row = bottom; row > top; row--) {
+					order[index++] = matrix[row][left];
+				}
+			}
+			left++;
+			right--;
+			top++;
+			bottom--;
+		}
+		return order;
+	}
 }
