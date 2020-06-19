985927deb6e4909ea5571668da40942b2bd08055
Zou Xiaodong <xiaodong.zou@luckincoffee.com>
2020-06-19 17:27:51 +0800

    125 commit

--- src/main/java/com/zxd/test/leetcode/IsPalindrome_125.java
+++ src/main/java/com/zxd/test/leetcode/IsPalindrome_125.java
@@ -27,34 +27,34 @@ public class IsPalindrome_125 {
 		System.out.println(isPalindrome_125.isPalindrome("race a car"));
 		System.out.println(isPalindrome_125.isPalindrome("         "));
 		System.out.println(isPalindrome_125.isPalindrome("0P"));
+		System.out.println(isPalindrome_125.isPalindrome("0"));
+		System.out.println(isPalindrome_125.isPalindrome(""));
 	}
 
 	public boolean isPalindrome(String s) {
 		int len = s.length();
-		if(len>1){
-			for(int l=0,r=len-1;l<=r;){
+		for(int l=0,r=len-1;l<=r;){
 //				if(!((s.charAt(l)>='a' && s.charAt(l)<='z') || (s.charAt(l)>='0' && s.charAt(l)<='9') || (s.charAt(l)>='A' && s.charAt(l)<='Z'))){
 //					l++;
 //					continue;
 //				}
-				if(!Character.isLetterOrDigit(s.charAt(l))){
-					l++;
-					continue;
-				}
+			if(!Character.isLetterOrDigit(s.charAt(l))){
+				l++;
+				continue;
+			}
 //				if(!((s.charAt(r)>='a' && s.charAt(r)<='z') || (s.charAt(r)>='0' && s.charAt(r)<='9') || (s.charAt(r)>='A' && s.charAt(r)<='Z'))){
 //					r--;
 //					continue;
 //				}
-				if(!Character.isLetterOrDigit(s.charAt(r))){
-					r--;
-					continue;
-				}
-				if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
-					return false;
-				}
-				l++;
+			if(!Character.isLetterOrDigit(s.charAt(r))){
 				r--;
+				continue;
 			}
+			if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
+				return false;
+			}
+			l++;
+			r--;
 		}
 		return true;
 	}
