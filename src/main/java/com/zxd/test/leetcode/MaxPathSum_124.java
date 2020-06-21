94956b411a64c214e371e7a2f1047ed393180882
xiaodong.zou <zxd_0825@163.com>
2020-06-21 17:47:05 +0800

    124 offical answer

--- src/main/java/com/zxd/test/leetcode/MaxPathSum_124.java
+++ src/main/java/com/zxd/test/leetcode/MaxPathSum_124.java
@@ -55,26 +55,27 @@ public class MaxPathSum_124 {
         System.out.println(maxPathSum_124.maxPathSum(root));
     }
 
+    int maxSum = Integer.MIN_VALUE;
+
     public int maxPathSum(TreeNode root) {
-        List<Integer> inOrderList = new ArrayList<>(10);
-        inOrder(root,inOrderList);
-        System.out.println(inOrderList);
-        int size = inOrderList.size();
-        int[] dp = new int[size];
-        dp[0] = inOrderList.get(0);
-        for(int i=1;i<size;i++){
-            dp[i] = inOrderList.get(i)>=0?(inOrderList.get(i-1)<0?(inOrderList.get(i-1)+dp[i-1]>=0?dp[i-1]+inOrderList.get(i-1)+inOrderList.get(i):inOrderList.get(i)):inOrderList.get(i)+dp[i-1]):(dp[i-1]>inOrderList.get(i)?dp[i-1]:inOrderList.get(i));
-        }
-        return dp[size-1];
+        maxGain(root);
+        return maxSum;
     }
 
-    private void inOrder(TreeNode node,List<Integer> inOrderList){
-        if(node == null){
-            return;
+    public int maxGain(TreeNode node) {
+        if (node == null) {
+            return 0;
         }
-        inOrder(node.left,inOrderList);
-        inOrderList.add(node.val);
-        inOrder(node.right,inOrderList);
+        // 递归计算左右子节点的最大贡献值
+        // 只有在最大贡献值大于 0 时，才会选取对应子节点
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+        // 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
+        int priceNewpath = node.val + leftGain + rightGain;
+        // 更新答案
+        maxSum = Math.max(maxSum, priceNewpath);
+        // 返回节点的最大贡献值
+        return node.val + Math.max(leftGain, rightGain);
     }
 }
 /**
