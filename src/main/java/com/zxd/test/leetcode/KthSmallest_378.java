b3629675dfb106ddb8857b827035416108336dff
xiaodong.zou <zxd_0825@163.com>
2020-07-02 14:25:49 +0800

    378 commit

--- src/main/java/com/zxd/test/leetcode/KthSmallest_378.java
+++ src/main/java/com/zxd/test/leetcode/KthSmallest_378.java
@@ -1,6 +1,8 @@
 package com.zxd.test.leetcode;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * @ClassName KthSmallest_378
@@ -42,6 +44,12 @@ public class KthSmallest_378 {
 
     }
 
+    /**
+     * 方法一:直接排序
+     * @param matrix
+     * @param k
+     * @return
+     */
     public int kthSmallest(int[][] matrix, int k) {
         int length = matrix.length;
         int[] nums = new int[length*length];
@@ -53,5 +61,66 @@ public class KthSmallest_378 {
         }
         Arrays.sort(nums);
         return nums[k-1];
+    }
+
+
+    /**
+     * 方法二：归并排序
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public int kthSmallest_2(int[][] matrix, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            pq.offer(new int[]{matrix[i][0], i, 0});
+        }
+        for (int i = 0; i < k - 1; i++) {
+            int[] now = pq.poll();
+            if (now[2] != n - 1) {
+                pq.offer(new int[]{matrix[now[1]][now[2] + 1], now[1], now[2] + 1});
+            }
+        }
+        return pq.poll()[0];
+    }
+    /**
+     * 方法三：二分查找
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public int kthSmallest_3(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (check(matrix, mid, k, n)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public boolean check(int[][] matrix, int mid, int k, int n) {
+        int i = n - 1;
+        int j = 0;
+        int num = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] <= mid) {
+                num += i + 1;
+                j++;
+            } else {
+                i--;
+            }
+        }
+        return num >= k;
     }
 }
