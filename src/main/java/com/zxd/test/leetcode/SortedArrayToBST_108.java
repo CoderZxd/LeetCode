b94cf719c8c588aa6839fadb60dc7d17651995af
xiaodong.zou <zxd_0825@163.com>
2020-07-03 15:08:13 +0800

    108 offical answer commit

--- src/main/java/com/zxd/test/leetcode/SortedArrayToBST_108.java
+++ src/main/java/com/zxd/test/leetcode/SortedArrayToBST_108.java
@@ -51,6 +51,30 @@ public class SortedArrayToBST_108 {
         root.right = sortedArrayToBST(rightNums);
         return root;
     }
+
+
+    /**
+     * 官方题解:中序遍历，总是选择中间位置右边的数字作为根节点
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST_1(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        // 总是选择中间位置右边的数字作为根节点
+        int mid = (left + right + 1) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
 }
 
 
