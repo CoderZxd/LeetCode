2d49a3fbf9a231f58b1600add27ea3222885dbc6
xiaodong.zou <zxd_0825@163.com>
2020-07-07 22:45:46 +0800

    112 commit

--- src/main/java/com/zxd/test/leetcode/HasPathSum_112.java
+++ src/main/java/com/zxd/test/leetcode/HasPathSum_112.java
@@ -26,12 +26,35 @@ package com.zxd.test.leetcode;
 public class HasPathSum_112 {
 
     public static void main(String[] args) {
-
+        TreeNode root = new TreeNode(1);
+        TreeNode root_l = new TreeNode(2);
+        TreeNode root_l_l = new TreeNode(11);
+        TreeNode root_l_l_l = new TreeNode(7);
+        TreeNode root_l_l_r = new TreeNode(2);
+        TreeNode root_r = new TreeNode(8);
+        TreeNode root_r_l = new TreeNode(13);
+        TreeNode root_r_r = new TreeNode(4);
+        TreeNode root_r_r_r = new TreeNode(1);
+        root.left = root_l;
+//        root.right = root_r;
+//        root_l.left = root_l_l;
+//        root_l_l.left = root_l_l_l;
+//        root_l_l.right = root_l_l_r;
+//        root_r.left = root_r_l;
+//        root_r.right = root_r_r;
+//        root_r_r.right = root_r_r_r;
+        HasPathSum_112 hasPathSum = new HasPathSum_112();
+        System.out.println(hasPathSum.hasPathSum(root,1));
     }
 
     public boolean hasPathSum(TreeNode root, int sum) {
-
-        return false;
+        if(root == null){
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 }
 
