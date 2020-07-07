0f1dd8b852a660859f6fcb1ddf60cc382b4f319a
xiaodong.zou <zxd_0825@163.com>
2020-07-07 22:14:58 +0800

    112 first commit

---
+++ src/main/java/com/zxd/test/leetcode/HasPathSum_112.java
@@ -0,0 +1,46 @@
+package com.zxd.test.leetcode;
+
+/**
+ * @ClassName HasPathSum_112
+ * @Description https://leetcode-cn.com/problems/path-sum/
+ * @Author xiaodong.zou
+ * @Date 2020/7/7 22:13
+ * @Version 1.0
+ */
+//112. 路径总和
+//        给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+//
+//        说明: 叶子节点是指没有子节点的节点。
+//
+//        示例:
+//        给定如下二叉树，以及目标和 sum = 22，
+//
+//         5
+//        / \
+//       4   8
+//      /   / \
+//     11  13  4
+//    /  \      \
+//   7    2      1
+//        返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+public class HasPathSum_112 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+
+        return false;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
