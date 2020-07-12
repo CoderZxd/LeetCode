ad3870dae2369c147dbd83f4f0f473f000f257e7
xiaodong.zou <zxd_0825@163.com>
2020-07-12 12:10:03 +0800

    174 commit

--- src/main/java/com/zxd/test/leetcode/CalculateMinimumHP_174.java
+++ src/main/java/com/zxd/test/leetcode/CalculateMinimumHP_174.java
@@ -1,5 +1,7 @@
 package com.zxd.test.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @ClassName CalculateMinimumHP_174
  * @Description https://leetcode-cn.com/problems/dungeon-game/
@@ -38,7 +40,18 @@ public class CalculateMinimumHP_174 {
 
     }
     public int calculateMinimumHP(int[][] dungeon) {
-
-        return 0;
+        int n = dungeon.length, m = dungeon[0].length;
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; ++i) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        dp[n][m - 1] = dp[n - 1][m] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) {
+                int minn = Math.min(dp[i + 1][j], dp[i][j + 1]);
+                dp[i][j] = Math.max(minn - dungeon[i][j], 1);
+            }
+        }
+        return dp[0][0];
     }
 }
