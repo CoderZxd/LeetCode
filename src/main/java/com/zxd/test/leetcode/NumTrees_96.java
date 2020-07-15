08b1eed297254b2e17aa00c35a02829f2fb42949
xiaodong.zou <zxd_0825@163.com>
2020-07-15 22:24:50 +0800

    96 commit

--- src/main/java/com/zxd/test/leetcode/NumTrees_96.java
+++ src/main/java/com/zxd/test/leetcode/NumTrees_96.java
@@ -26,8 +26,36 @@ public class NumTrees_96 {
 
     }
 
+    /**
+     * 动态规划
+     * @param n
+     * @return
+     */
     public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
 
-        return 0;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+
+    /**
+     * 方法二：数学
+     * 事实上我们在方法一中推导出的 G(n)函数的值在数学上被称为卡塔兰数
+     * @param n
+     * @return
+     */
+    public int numTrees_1(int n) {
+        // 提示：我们在这里需要用 long 类型防止计算过程中的溢出
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
     }
 }
