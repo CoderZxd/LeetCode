33245e8f4b13f969ab1ac83226c10d6126aedc4b
zouxiaodong <zouxd@chip-cloud.com>
2020-07-31 08:23:54 +0800

    08.03 for offer commit

---
+++ src/main/java/com/zxd/test/leetcode/FindMagicIndex_For_Offer_08_03.java
@@ -0,0 +1,42 @@
+package com.zxd.test.leetcode;
+
+/**
+ * @Description https://leetcode-cn.com/problems/magic-index-lcci/
+ * @Author zouxiaodong
+ * @Date 2020/07/31 8:19
+ */
+//面试题 08.03. 魔术索引
+//        魔术索引。
+//        在数组A[0...n-1]中，有所谓的魔术索引，满足条件A[i] = i。给定一个有序整数数组，编写一种方法找出魔术索引，若有的话，在数组A中找出一个魔术索引，
+//        如果没有，则返回-1。若有多个魔术索引，返回索引值最小的一个。
+//
+//        示例1:
+//
+//        输入：nums = [0, 2, 3, 4, 5]
+//        输出：0
+//        说明: 0下标的元素为0
+//        示例2:
+//
+//        输入：nums = [1, 1, 1]
+//        输出：1
+
+//        提示:
+//        nums长度在[1, 1000000]之间
+public class FindMagicIndex_For_Offer_08_03 {
+
+    public static void main(String[] args) {
+        FindMagicIndex_For_Offer_08_03 offer = new FindMagicIndex_For_Offer_08_03();
+        System.out.println(offer.findMagicIndex(new int[]{0, 2, 3, 4, 5}));
+        System.out.println(offer.findMagicIndex(new int[]{1,1,1}));
+    }
+
+    public int findMagicIndex(int[] nums) {
+        int len = nums.length;
+        for(int i=0;i<len;i++){
+            if(nums[i] == i){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
