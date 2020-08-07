0e06667fb11e0e893cce8207c820d48e4f62feba
zouxiaodong <zouxd@chip-cloud.com>
2020-08-07 08:54:34 +0800

    100 second committ

--- src/main/java/com/zxd/test/leetcode/IsSameTree_100.java
+++ src/main/java/com/zxd/test/leetcode/IsSameTree_100.java
@@ -58,13 +58,11 @@ public class IsSameTree_100 {
     }
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if((p == null && q != null) || (p != null && q == null)){
-            return false;
-        }
         if(p == null && q == null){
             return true;
-        }
-        if(p.val != q.val){
+        }else if(p == null || q == null){
+            return false;
+        }else if(p.val != q.val){
             return false;
         }
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
