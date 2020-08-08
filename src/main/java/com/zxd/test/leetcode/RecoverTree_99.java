6f7e5271ccb1eb23bf9e4106f898787cc5d8a779
xiaodong.zou <zxd_0825@163.com>
2020-08-09 00:08:19 +0800

    99 commit

--- src/main/java/com/zxd/test/leetcode/RecoverTree_99.java
+++ src/main/java/com/zxd/test/leetcode/RecoverTree_99.java
@@ -1,5 +1,8 @@
 package com.zxd.test.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @ClassName RecoverTree_99
  * @Description https://leetcode-cn.com/problems/recover-binary-search-tree/
@@ -48,11 +51,61 @@ package com.zxd.test.leetcode;
 public class RecoverTree_99 {
 
     public static void main(String[] args) {
-
+        TreeNode root = new TreeNode(3);
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(4);
+        TreeNode r_l = new TreeNode(2);
+        root.left = left;
+        root.right = right;
+        right.left = r_l;
+        RecoverTree_99 recoverTree = new RecoverTree_99();
+        recoverTree.recoverTree(root);
+        System.out.println();
     }
 
     public void recoverTree(TreeNode root) {
+        List<Integer> nums = new ArrayList<Integer>();
+        inOrder(root, nums);
+        int[] swapped = findTwoSwapped(nums);
+        recover(root, 2, swapped[0], swapped[1]);
+    }
 
+    public int[] findTwoSwapped(List<Integer> nums) {
+        int n = nums.size();
+        int x = -1, y = -1;
+        for (int i = 0; i < n - 1; ++i) {
+            if (nums.get(i + 1) < nums.get(i)) {
+                y = nums.get(i + 1);
+                if (x == -1) {
+                    x = nums.get(i);
+                } else {
+                    break;
+                }
+            }
+        }
+        return new int[]{x, y};
+    }
+
+    public void recover(TreeNode root, int count, int x, int y) {
+        if (root != null) {
+            if (root.val == x || root.val == y) {
+                root.val = root.val == x ? y : x;
+                if (--count == 0) {
+                    return;
+                }
+            }
+            recover(root.right, count, x, y);
+            recover(root.left, count, x, y);
+        }
+    }
+
+    private void inOrder(TreeNode treeNode,List<Integer> inOrderList){
+        if(treeNode == null){
+            return;
+        }
+        inOrder(treeNode.left,inOrderList);
+        inOrderList.add(treeNode.val);
+        inOrder(treeNode.right,inOrderList);
     }
 }
 /**
