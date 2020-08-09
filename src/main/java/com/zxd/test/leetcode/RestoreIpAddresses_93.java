177fbd432b2a1ac21321e8f28107571a20fe707f
xiaodong.zou <zxd_0825@163.com>
2020-08-09 16:54:40 +0800

    93 init

---
+++ src/main/java/com/zxd/test/leetcode/RestoreIpAddresses_93.java
@@ -0,0 +1,29 @@
+package com.zxd.test.leetcode;
+
+import java.util.List;
+
+/**
+ * @ClassName RestoreIpAddresses_93
+ * @Description https://leetcode-cn.com/problems/restore-ip-addresses/
+ * @Author xiaodong.zou
+ * @Date 2020/8/9 16:53
+ * @Version 1.0
+ */
+//93. 复原IP地址
+//        给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+//        有效的 IP 地址正好由四个整数（每个整数位于 0 到 255 之间组成），整数之间用 '.' 分隔。
+//
+//        示例:
+//        输入: "25525511135"
+//        输出: ["255.255.11.135", "255.255.111.35"]
+public class RestoreIpAddresses_93 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<String> restoreIpAddresses(String s) {
+
+        return null;
+    }
+}
