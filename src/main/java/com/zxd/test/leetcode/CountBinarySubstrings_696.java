da379aa06c1df960406c52a20593abbc44ea1b7e
zouxiaodong <zouxd@chip-cloud.com>
2020-08-10 08:58:15 +0800

    696 commit

--- src/main/java/com/zxd/test/leetcode/CountBinarySubstrings_696.java
+++ src/main/java/com/zxd/test/leetcode/CountBinarySubstrings_696.java
@@ -1,5 +1,8 @@
 package com.zxd.test.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description https://leetcode-cn.com/problems/count-binary-substrings/
  * @Author zouxiaodong
@@ -34,8 +37,30 @@ public class CountBinarySubstrings_696 {
 
     }
 
+//    方法一：按字符分组
+//
+//    我们可以将字符串 s 按照 0 和 1 的连续段分组，存在counts 数组中，例如 s=00111011，可以得到这样的counts 数组：counts={2,3,1,2}。
+//
+//    这里 counts 数组中两个相邻的数一定代表的是两种不同的字符。假设counts 数组中两个相邻的数字为 u 或者 v，
+//    它们对应着 u 个 0 和 v 个 1，或者 u 个 1 和 v 个 0。它们能组成的满足条件的子串数目为 min{u,v}，即一对相邻的数字对答案的贡献。
+//
+//    我们只要遍历所有相邻的数对，求它们的贡献总和，即可得到答案。
     public int countBinarySubstrings(String s) {
-
-        return 0;
+        List<Integer> counts = new ArrayList<Integer>();
+        int ptr = 0, n = s.length();
+        while (ptr < n) {
+            char c = s.charAt(ptr);
+            int count = 0;
+            while (ptr < n && s.charAt(ptr) == c) {
+                ++ptr;
+                ++count;
+            }
+            counts.add(count);
+        }
+        int ans = 0;
+        for (int i = 1; i < counts.size(); ++i) {
+            ans += Math.min(counts.get(i), counts.get(i - 1));
+        }
+        return ans;
     }
 }
