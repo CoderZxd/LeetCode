e0699bd6bb9dbe6b65f2385d5bbb4b0fea26b890
zouxiaodong <zouxd@chip-cloud.com>
2020-08-17 08:31:04 +0800

    110 commit

---
+++ src/main/java/com/zxd/test/leetcode/IsBalanced_110.java
@@ -0,0 +1,84 @@
+package com.zxd.test.leetcode;
+
+/**
+ * @Description https://leetcode-cn.com/problems/balanced-binary-tree/
+ * @Author zouxiaodong
+ * @Date 2020/08/17 8:18
+ */
+//110. 平衡二叉树
+//        给定一个二叉树，判断它是否是高度平衡的二叉树。
+//
+//        本题中，一棵高度平衡二叉树定义为：
+//
+//        一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+//
+//        示例 1:
+//
+//        给定二叉树 [3,9,20,null,null,15,7]
+//
+//         3
+//        / \
+//       9  20
+//         /  \
+//        15   7
+//        返回 true 。
+//
+//        示例 2:
+//
+//        给定二叉树 [1,2,2,3,3,null,null,4,4]
+//
+//         1
+//        / \
+//       2   2
+//      / \
+//     3   3
+//    / \
+//   4   4
+//        返回 false 。
+public class IsBalanced_110 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode root_l = new TreeNode(9);
+        TreeNode root_r = new TreeNode(20);
+        TreeNode root_r_l = new TreeNode(15);
+        TreeNode root_r_r = new TreeNode(7);
+        root_r.left = root_r_l;
+        root_r.right = root_r_r;
+        root.left = root_l;
+        root.right = root_r;
+        IsBalanced_110 isBalanced = new IsBalanced_110();
+        System.out.println(isBalanced.isBalanced(root));
+        TreeNode root2 = new TreeNode(1);
+        TreeNode root2_l = new TreeNode(2);
+        TreeNode root2_r = new TreeNode(2);
+        TreeNode root2_l_l = new TreeNode(3);
+        TreeNode root2_l_r = new TreeNode(3);
+        TreeNode root2_l_l_l = new TreeNode(4);
+        TreeNode root2_l_l_r = new TreeNode(4);
+        root2_l_l.left = root2_l_l_l;
+        root2_l_r.right = root2_l_l_r;
+        root2_l.left = root2_l_l;
+        root2_l.right = root2_l_r;
+        root2.left = root2_l;
+        root2.right = root2_r;
+        System.out.println(isBalanced.isBalanced(root2));
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        if(Math.abs(dfs(root.left) - dfs(root.right)) <= 1){
+            return true;
+        }
+        return false;
+    }
+
+    private int dfs(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        return Math.max(dfs(node.left),dfs(node.right))+1;
+    }
+}
