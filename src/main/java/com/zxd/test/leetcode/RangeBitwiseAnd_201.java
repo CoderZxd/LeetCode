076ae0810f32f730d717aea389928eba5ded5057
xiaodong.zou <zxd_0825@163.com>
2020-08-23 12:13:15 +0800

    201 first comnnit

---
+++ src/main/java/com/zxd/test/leetcode/RangeBitwiseAnd_201.java
@@ -0,0 +1,30 @@
+package com.zxd.test.leetcode;
+
+/**
+ * @ClassName RangeBitwiseAnd_201
+ * @Description https://leetcode-cn.com/problems/bitwise-and-of-numbers-range/
+ * @Author xiaodong.zou
+ * @Date 2020/8/23 12:11
+ * @Version 1.0
+ */
+//201. 数字范围按位与
+//        给定范围 [m, n]，其中 0 <= m <= n <= 2147483647，返回此范围内所有数字的按位与（包含 m, n 两端点）。
+//
+//        示例 1:
+//
+//        输入: [5,7]
+//        输出: 4
+//        示例 2:
+//
+//        输入: [0,1]
+//        输出: 0
+public class RangeBitwiseAnd_201 {
+
+    public static void main(String[] args) {
+    }
+
+    public int rangeBitwiseAnd(int m, int n) {
+
+        return 0;
+    }
+}
