716bf38241063d18cc1e67c393ed7ca292441464
xiaodong.zou <zxd_0825@163.com>
2020-08-23 13:05:58 +0800

    201 commit

--- src/main/java/com/zxd/test/leetcode/RangeBitwiseAnd_201.java
+++ src/main/java/com/zxd/test/leetcode/RangeBitwiseAnd_201.java
@@ -21,10 +21,33 @@ package com.zxd.test.leetcode;
 public class RangeBitwiseAnd_201 {
 
     public static void main(String[] args) {
+        RangeBitwiseAnd_201 rangeBitwiseAnd_201 = new RangeBitwiseAnd_201();
+        System.out.println(rangeBitwiseAnd_201.rangeBitwiseAnd(5,7));
     }
 
+//    方法一：位移
+//            思路
+//
+//    鉴于上述问题的陈述，我们的目的是求出两个给定数字的二进制字符串的公共前缀，这里给出的第一个方法是采用位移操作。
+//
+//    我们的想法是将两个数字不断向右移动，直到数字相等，即数字被缩减为它们的公共前缀。然后，通过将公共前缀向左移动，将零添加到公共前缀的右边以获得最终结果。
+//
+//
+//
+//    算法
+//
+//    如上述所说，算法由两个步骤组成：
+//
+//    我们通过右移，将两个数字压缩为它们的公共前缀。在迭代过程中，我们计算执行的右移操作数。
+//    将得到的公共前缀左移相同的操作数得到结果。
     public int rangeBitwiseAnd(int m, int n) {
-
-        return 0;
+        int shift = 0;
+        // 找到公共前缀
+        while (m < n) {
+            m >>= 1;
+            n >>= 1;
+            ++shift;
+        }
+        return m << shift;
     }
 }
