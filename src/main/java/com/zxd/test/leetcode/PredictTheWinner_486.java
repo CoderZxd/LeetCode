46cea004a9020370b8cca80b63b172c3c1aa1839
xiaodong.zou <zxd_0825@163.com>
2020-09-02 19:24:01 +0800

    486 commit

--- src/main/java/com/zxd/test/leetcode/PredictTheWinner_486.java
+++ src/main/java/com/zxd/test/leetcode/PredictTheWinner_486.java
@@ -37,9 +37,42 @@ public class PredictTheWinner_486 {
 
     }
 
+    /**
+     * 方法一 递归
+     * @param nums
+     * @return
+     */
     public boolean PredictTheWinner(int[] nums) {
+        return total(nums, 0, nums.length - 1, 1) >= 0;
+    }
 
-        return false;
+    public int total(int[] nums, int start, int end, int turn) {
+        if (start == end) {
+            return nums[start] * turn;
+        }
+        int scoreStart = nums[start] * turn + total(nums, start + 1, end, -turn);
+        int scoreEnd = nums[end] * turn + total(nums, start, end - 1, -turn);
+        return Math.max(scoreStart * turn, scoreEnd * turn) * turn;
+    }
+
+
+    /**
+     * 方法二 动态规划
+     * @param nums
+     * @return
+     */
+    public boolean PredictTheWinner_1(int[] nums) {
+        int length = nums.length;
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] >= 0;
     }
 
 }
