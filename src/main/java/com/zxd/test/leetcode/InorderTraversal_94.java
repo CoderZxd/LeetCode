38dab02360fda3cd8b169dfcab01b1338124226e
zouxiaodong <zouxd@chip-cloud.com>
2020-09-14 08:44:54 +0800

    94 commit

--- src/main/java/com/zxd/test/leetcode/InorderTraversal_94.java
+++ src/main/java/com/zxd/test/leetcode/InorderTraversal_94.java
@@ -1,6 +1,8 @@
 package com.zxd.test.leetcode;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -24,8 +26,6 @@ import java.util.List;
 //        进阶: 递归算法很简单，你可以通过迭代算法完成吗？
 public class InorderTraversal_94 {
 
-    private List<Integer> resultList = new ArrayList<>();
-
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         TreeNode root_r = new TreeNode(2);
@@ -36,6 +36,8 @@ public class InorderTraversal_94 {
         System.out.println(inorderTraversal_94.inorderTraversal(root));
 
     }
+
+    private List<Integer> resultList = new ArrayList<>();
 
     public List<Integer> inorderTraversal(TreeNode root) {
         inOrder(root);
@@ -56,6 +58,29 @@ public class InorderTraversal_94 {
         inOrder(node.left);
         resultList.add(node.val);
         inOrder(node.right);
+    }
+
+
+    /**
+     * @Author zouxiaodong
+     * @Description 迭代
+     * @Date 2020/09/14 8:42
+     * @Param [root]
+     * @return java.util.List<java.lang.Integer>
+     **/
+    public List<Integer> inorderTraversal_1(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNode> stk = new LinkedList<TreeNode>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
     }
 }
 /**
