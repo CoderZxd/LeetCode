a433d3170242f8ddb87c9f369eb546da0befc2ab
zouxiaodong <zouxd@chip-cloud.com>
2020-10-22 09:36:59 +0800

    763 commit

--- src/main/java/com/zxd/test/leetcode/PartitionLabels_763.java
+++ src/main/java/com/zxd/test/leetcode/PartitionLabels_763.java
@@ -28,6 +28,7 @@ public class PartitionLabels_763 {
     public static void main(String[] args) {
         PartitionLabels_763 partitionLabels_763 = new PartitionLabels_763();
         System.out.println(partitionLabels_763.partitionLabels("ababcbacadefegdehijhklij"));
+        System.out.println(partitionLabels_763.partitionLabels_1("ababcbacadefegdehijhklij"));
 
     }
 
@@ -35,22 +36,46 @@ public class PartitionLabels_763 {
         List<Integer> resultList = new ArrayList<>(10);
         int len = S.length();
         for(int i=0;i<len;){
-            char tempChar = S.charAt(i);
-            int index = S.lastIndexOf(tempChar);
+            int index = S.lastIndexOf(S.charAt(i));
             if(index == len){
                 resultList.add(len-index+1);
                 return resultList;
             }
             for(int j=i+1;j<index;j++){
-                char tempJ = S.charAt(j);
-                int lastIndexJ = S.lastIndexOf(tempJ);
+                int lastIndexJ = S.lastIndexOf(S.charAt(j));
                 if(lastIndexJ > index){
                     index = lastIndexJ;
                 }
             }
             resultList.add(index-i+1);
-            i=index+1;
+            i = index + 1;
         }
         return resultList;
+    }
+
+    /**
+     * @Author zouxiaodong
+     * @Description 贪心算法
+     * @Date 2020/10/22 9:34
+     * @Param [S]
+     * @return java.util.List<java.lang.Integer>
+     **/
+    public List<Integer> partitionLabels_1(String S) {
+        int[] last = new int[26];
+        int length = S.length();
+        for (int i = 0; i < length; i++) {
+            last[S.charAt(i) - 'a'] = i;
+        }
+        List<Integer> partition = new ArrayList<Integer>();
+        int start = 0, end = 0;
+        for (int i = 0; i < length; i++) {
+            end = Math.max(end, last[S.charAt(i) - 'a']);
+            //精髓所在
+            if (i == end) {
+                partition.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
     }
 }
