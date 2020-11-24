db8719faf3f87afb234afc4b66b57e3f1213e20a
zouxiaodong <zouxd@chip-cloud.com>
2020-11-24 08:36:31 +0800

    222 add offical answer

--- src/main/java/com/zxd/test/leetcode/CountNodes_222.java
+++ src/main/java/com/zxd/test/leetcode/CountNodes_222.java
@@ -46,6 +46,50 @@ public class CountNodes_222 {
         }
         return 1+countNodes(root.left)+countNodes(root.right);
     }
+
+
+    /**
+     * @Author zouxiaodong
+     * @Description 方法一：二分查找 + 位运算
+     * @Date 2020/11/24 8:31
+     * @Param [root]
+     * @return int
+     **/
+    public int countNodes_offical(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
 }
 /**
  * Definition for a binary tree node.
