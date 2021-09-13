49155d80278e45c76f974d12700c6c49520d7a5b
zouxiaodong <zouxd@chip-cloud.com>
2021-09-13 11:13:25 +0800

    447 commit

---
+++ src/main/java/com/zxd/test/leetcode/NumberOfBoomerangs_447.java
@@ -0,0 +1,60 @@
+package com.zxd.test.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description https://leetcode-cn.com/problems/number-of-boomerangs/
+ * @Author zouxiaodong
+ * @Date 2021/09/13 9:45
+ */
+//447. 回旋镖的数量
+//        给定平面上 n 对 互不相同 的点 points ，其中 points[i] = [xi, yi] 。回旋镖 是由点 (i, j, k) 表示的元组 ，
+//        其中 i 和 j 之间的距离和 i 和 k 之间的距离相等（需要考虑元组的顺序）。
+//        返回平面上所有回旋镖的数量。
+//
+//
+//        示例 1：
+//        输入：points = [[0,0],[1,0],[2,0]]
+//        输出：2
+//        解释：两个回旋镖为 [[1,0],[0,0],[2,0]] 和 [[1,0],[2,0],[0,0]]
+
+//        示例 2：
+//        输入：points = [[1,1],[2,2],[3,3]]
+//        输出：2
+
+//        示例 3：
+//        输入：points = [[1,1]]
+//        输出：0
+//
+//
+//        提示：
+//        n == points.length
+//        1 <= n <= 500
+//        points[i].length == 2
+//        -10^4 <= xi, yi <= 10^4
+//        所有点都 互不相同
+public class NumberOfBoomerangs_447 {
+
+    public static void main(String[] args) {
+        NumberOfBoomerangs_447 numberOfBoomerangs_447 = new NumberOfBoomerangs_447();
+        int[][] points = new int[][]{{1,1},{2,2},{3,3}};
+        System.out.println(numberOfBoomerangs_447.numberOfBoomerangs(points));
+    }
+
+    public int numberOfBoomerangs(int[][] points) {
+        int ans = 0;
+        for (int[] p : points) {
+            Map<Integer, Integer> cnt = new HashMap<>(16);
+            for (int[] q : points) {
+                int dis = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]);
+                cnt.put(dis, cnt.getOrDefault(dis, 0) + 1);
+            }
+            for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
+                int m = entry.getValue();
+                ans += m * (m - 1);
+            }
+        }
+        return ans;
+    }
+}
